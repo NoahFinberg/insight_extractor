5952a463e69aee0a31fe643131df8e1f86cfa4f8
***** <gsood07@gmail.com>
2021-01-16 22:44:26 -0800

    bump version + fix test

--- insight_extractor/tests/test_insight_extractor.py
+++ insight_extractor/tests/test_insight_extractor.py
@@ -9,27 +9,29 @@ Tests for insight_extractor
 import os
 import shutil
 import unittest
+import numpy as np
 import pandas as pd
 from insight_extractor.pipeline import extract_insights
 from . import capture
 
 
-class TestCensusLn(unittest.TestCase):
+class TestInsightExtractor(unittest.TestCase):
 
     def setUp(self):
-        sentences = [
+        self.sentences = [
     'According to the most recent statistics, more than a million people a year are arrested for simple drug possession in the United States -- and more than half a million of those arrests are for marijuana possession.',
     'One study found that for cancer patients considering experimental chemotherapy, trust in their physician was one of the most important reasons they enrolled in a clinical trial -- on par with the belief that the treatment would be effective.',
     'Senate leaders were working to agree on a dual track to try the departing president at the same time it considered the agenda of the incoming one, an exercise never tried before.',
 ]
-        self = sentences
 
     def tearDown(self):
         pass
 
     def test_insight_extractor(self):
-        predictions = extract_insights(sentences)
-        self.assertTrue(all(predictions == [0.28326818, 0.3710433, 0.9886193]))
+        predictions = extract_insights(self.sentences)
+        ref = np.array([[0.2832682 , 0.7167318 ], [0.3710433 , 0.6289567 ], [0.9886193 , 0.01138071]])
+        self.assertTrue(np.allclose(predictions, ref))
 
 if __name__ == '__main__':
     unittest.main()
+
