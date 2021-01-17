7e045fdd3357f3f421f6dbfee4f4b2cbd2d9bff1
***** <gsood07@gmail.com>
2021-01-16 22:14:47 -0800

    fix package dir

--- setup.py
+++ setup.py
@@ -41,12 +41,11 @@ except FileNotFoundError:
 
 # Load the package's __version__.py module as a dictionary.
 ROOT_DIR = Path(__file__).resolve().parent
-PACKAGE_DIR = ROOT_DIR / NAME
+PACKAGE_DIR = NAME
 about = {}
 with open(PACKAGE_DIR / 'VERSION') as f:
     _version = f.read().strip()
     about['__version__'] = _version
-
 
 # Where the magic happens:
 setup(
