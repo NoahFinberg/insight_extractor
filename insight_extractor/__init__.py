5f4fb9a5bbfc517ec0a4e853d61e9255b665ea33
***** <gsood07@gmail.com>
2021-01-16 22:33:54 -0800

    remove predict

--- insight_extractor/__init__.py
+++ insight_extractor/__init__.py
@@ -1,6 +1,5 @@
 from .preprocessors import clean_doc
 from .preprocessors import clean_docs
 from .pipeline import extract_insights
-from .predict import predict
 
-__all__ = ['clean_doc', 'clean_docs', 'extract_insights', 'predict']
+__all__ = ['clean_doc', 'clean_docs', 'extract_insights']
