89f5c6af32b0cb44965a782ee4eb5fa5d35b2c0e
***** <gsood07@gmail.com>
2021-01-16 22:03:20 -0800

    public functions

---
+++ insight_extractor/__init__.py
@@ -0,0 +1,6 @@
+from .preprocessors import clean_doc
+from .preprocessors import clean_docs
+from .pipeline import extract_insights
+from .predict import predict
+
+__all__ = ['clean_doc', 'clean_docs', 'extract_insights', 'predict']
